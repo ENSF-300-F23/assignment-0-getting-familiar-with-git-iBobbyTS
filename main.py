d87af18ec20b847981cc351b8449cb06a31bd6ac
ibobby <ibobbyts@gmail.com>
2023-09-19 21:17:06 -0600

    Add coding standards

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Remember to follow these coding standards:
+Put a space( ) after comma(,)
+Put a space before and after assignment(=) and comparison(==, !=, etc.)
+Put a space before and after arithmetic operators(+, -, *, /, etc.)
+Don't put a space at the end of a line
+Leave 2 blank lines between functions
+"""
