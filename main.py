0726ec5a0116947757eb13f1adee0dd41ad93b96
jabezchowdhury <jabez.chowdhury@ucalgary.ca>
2023-09-25 15:01:35 -0600

    added assignedtoj.py to main file

--- main.py
+++ main.py
@@ -29,3 +29,28 @@ def evaluate(number_list, operation_list):
     else:
         temp = operation(operation_list[0], number_list[0], number_list[1])
         return operation(operation_list[1], temp, number_list[2])
+    
+
+def myAdd(a, b):
+    return a + b
+
+def mySub(a, b):
+    return a - b
+
+def myMul(a, b):
+    return a * b
+
+def myDiv(a, b): 
+    if b != 0:
+        return a // b
+    else:
+        return "division by zero is not allowed."
+
+def validint(val):
+    while True:
+        user_input = input(val)
+    
+    if user_input.lstrip('-').isdigit():
+        return int(user_input)
+    else:
+        print('invalid input value.')
